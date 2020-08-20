ca99a28661866f204fffee309395b7e61491e165
Юрий А. Денисов <yudenisov@mail.ru>
2020-08-20 05:50:39 +0400

    Transfer

--- Util/UNSECURE_ALL.cmd
+++
@@ -1,7 +0,0 @@
-call %PUB1%\Util\Unsecure-001.cmd
-call %PUB1%\Util\Unsecure-002.cmd
-call %PUB1%\Unsecure-003.cmd
-call %PUB1%\Util\Unsecure-004.cmd
-call %PUB1%\Util\run-update-win-000.bat
-call %PUB1%\Util\unsecure-winrm-client.cmd
-call %PUB1%\Util\unsecure-winrm-service.cmd
